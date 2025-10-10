d6bedcab0405d5f6a764f23827e1cfa68dea1dc1
Cider8 <kajalkri2323@gmail.com>
2025-10-10 23:29:25 +0530

    rewrite  cv section

--- src/components/CVSection.jsx
+++ src/components/CVSection.jsx
@@ -1,5 +1,5 @@
 export const CVSection = () => {
-    const cvPath = "/Kajal_Kri_resume.pdf"; 
+    const cvPath = "/Kajal_Kri_Resume.pdf"; 
     return (
         <section id="cv" className="py-20 px-6 lg:px-24">
             <div className="container mx-auto max-w-4xl text-center">
